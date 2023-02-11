b5f832a01c5cb68ca2e10c78bc5d403331cad137
lukrydz <luk.rydzewski@gmail.com>
2023-02-11 18:25:36 +0100

    Roles added

--- src/main/java/pl/sda/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/pl/sda/springsecurity/config/SecurityConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pl.sda.springsecurity.user.RoleType;
+
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
@@ -29,6 +31,8 @@ public class SecurityConfiguration {
                 .permitAll()
                 .requestMatchers(toH2Console())
                 .permitAll()
+                .requestMatchers("/api/v1/dummy")
+                .hasAuthority("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
